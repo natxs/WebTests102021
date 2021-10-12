b71be11ee2e37de42f4f21d693da7f367871ba3e
nvatstx <natalia.vats@gmail.com>
2021-10-11 21:30:36 -0500

    First commit for WebTests102021 project

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebTest {
+
+    @Test
+    public void testFirst() throws InterruptedException {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\chromedriver\\chromedriver_win32 (2)\\chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "http://www.99-bottles-of-beer.net/";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+        Thread.sleep(3000);
+
+        String actualResult = driver.getCurrentUrl();
+
+
+        driver.close();
+        driver.quit();
+    }
+}
