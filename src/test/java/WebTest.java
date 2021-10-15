7890e3b9fe6857bf3c197ec1aebd561e61932d49
nvatstx <natalia.vats@gmail.com>
2021-10-14 22:25:58 -0500

    testVerifyHeaderText was added

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -22,4 +22,23 @@ public class WebTest {
         driver.close();
         driver.quit();
     }
+
+    @Test
+    public void testVerifyHeaderText() throws InterruptedException {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\chromedriver\\chromedriver_win32 (2)\\chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Welcome to 99 Bottles of Beer ";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        String actualResult = driver.getTitle();
+
+        driver.close();
+        driver.quit();
+    }
 }
